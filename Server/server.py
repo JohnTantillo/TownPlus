58251ce1edf6d602c1828c69733f44a715293a33
johndunaske <johnduna@buffalo.edu>
2020-02-08 16:17:54 -0500

    Fixed sensorToServer
    :gb:

--- Server/server.py
+++ Server/server.py
@@ -2,4 +2,10 @@ import bottle
 
 @bottle.route("/")
 def home():
-    return bottle.static_file("C:\Users\J_Dun\Documents\GitHub\TownPlus\htmlFiles\home.html", root="C:\\Users\\J_Dun\\Documents\\GitHub\\TownPlus")
+    return bottle.static_file("htmlFiles\\home.html", root="C:\\Users\\J_Dun\\Documents\\GitHub\\TownPlus")
+
+@bottle.route("/sensorData")
+def sensor():
+    
+
+bottle.run(host="0.0.0.0",port="8080",debug=True)
--- UltraSonicCode/sensorToServer.py
+++ UltraSonicCode/sensorToServer.py
@@ -1,15 +1,14 @@
 import serial
-import io
 
-with serial.Serial() as ser:
-    ser.baudrate = 9600
-    ser.port = 'COM1'
-    ser.open()
+ser = serial.Serial(
+ser.baudrate = 9600,
+ser.port = '/dev/ttyACM0'
+)
 
-sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 
 running = True
 while running:
-    sio.flush()
-    data = sio.readline()
-    print(data)
+    data = ser.readline()
+    data = data.rstrip()
+    stringData = str(data)
+    print(stringData)
