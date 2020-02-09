543acb99219163f19ac847664d0e3a62278c28e1
johndunaske <johnduna@buffalo.edu>
2020-02-08 22:19:11 -0500

    the mexican beeping song is playing

--- Server/server.py
+++ Server/server.py
@@ -13,18 +13,22 @@ def home():
 @bottle.post("/sensorData")
 def sensor():
     print("adding new server data")
-    now = datetime.now()
-    dt_string = now.strftime("%H:%M:%S")
+    # now = datetime.now()
+    # dt_string = now.strftime("%H:%M:%S")
     client = pymongo.MongoClient("mongodb+srv://johnduna:100741Vcs@distances-mh9hl.mongodb.net/test?retryWrites=true&w=majority")
     db = client.Distances
     posts = db.dist
     dist = bottle.request.forms.get('dist')
-    posts.insert_one({"dist":float(dist), "id":dt_string})
+    posts.insert_one({"dist":float(dist))
 
 
 @bottle.route("/scala")
 def toScala():
     print("scala pulled")
-    return curDist[len(curDist)]
+    client = pymongo.MongoClient("mongodb+srv://johnduna:100741Vcs@distances-mh9hl.mongodb.net/test?retryWrites=true&w=majority")
+    db = client.Distances
+    posts = db.dist
+    data = posts.find().sort({_id:-1})
+    return data[0]
 
 bottle.run(host="0.0.0.0",port="8080",debug=True)
