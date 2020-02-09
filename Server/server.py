ec81053b1e87cf6d2791aa0382325913a677dd41
John Tantillo <johntantillo1@Johns-MacBook-Pro-3.local>
2020-02-09 07:08:43 -0500

    l

--- Server/server.py
+++ Server/server.py
@@ -30,7 +30,7 @@ def toScala():
     client = pymongo.MongoClient("mongodb+srv://johnduna:100741Vcs@distances-mh9hl.mongodb.net/test?retryWrites=true&w=majority")
     db = client.Distances
     posts = db.dist
-    data = posts.getCollection().findOne()
+    data = posts.find_one()
     if float(data.dist) <= 100:
         return json.dumps(True)
     else:
