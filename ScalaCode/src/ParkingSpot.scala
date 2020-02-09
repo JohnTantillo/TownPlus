27e2ecf409a8a679b80cd0f75249ce520858eb37
John Tantillo <johntantillo1@Johns-MacBook-Pro-3.local>
2020-02-09 12:10:49 -0500

    new

--- ScalaCode/src/ParkingSpot.scala
+++ ScalaCode/src/ParkingSpot.scala
@@ -3,7 +3,7 @@ class ParkingSpot( var ID: (Int, Int) ) {
   var filled = false
 
   def checkFilled(): Unit = {
-    val response = scala.io.Source.fromURL("http://165.227.223.64/scala").mkString
+    val response = scala.io.Source.fromURL("http://178.128.238.139/scala").mkString
     println(response)
     if( response == "true" )
       filled = true
