f29748c913f8d9a0914cf1c78abc62e09bb64005
johndunaske <johnduna@buffalo.edu>
2020-02-09 10:02:21 -0500

    oof

--- UltraSonicCode/sensorToServer.py
+++ UltraSonicCode/sensorToServer.py
@@ -4,6 +4,7 @@ import ssl
 
 nonMagicNumber = 100
 count = 0
+count2 = 0
 
 ser = serial.Serial(
 baudrate = 9600,
@@ -19,11 +20,16 @@ while running:
     data = data.replace("\'", "")
     print(data, flush=True)
     if data != "Out of range":
+        count2 = 0
         count = count + 1
         if count >= 200:
             count = 0
             print("sending to server")
             r = requests.post("http://165.227.223.64/sensorData", data = {'dist':float(data)})
     else:
-        r = requests.post("http://165.227.223.64/sensorData", data = {'dist':-1})
+        count = 0
+        count2 = count2 + 1
+        if count2 >= 200:
+            count2 = 0
+            r = requests.post("http://165.227.223.64/sensorData", data = {'dist':-1})
 #TODO: only send when there is a consistent change
